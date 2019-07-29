b0c75a926a567fd54fbdcbbaa923c538a6bd8773
phylib <phylib@users.noreply.github.com>
2019-07-29 16:38:37 +0200

    Improve installation instructions, add script for calculating overhead

--- result-analysis/install.sh
+++ result-analysis/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Install wireshark and NDN dissector
-sudo apt install tshark wireshark
+sudo apt install tshark wireshark python3-setuptools
 git clone https://github.com/named-data/ndn-tools.git
 cd ndn-tools/tools/dissect-wireshark
 sudo mkdir /usr/local/share/ndn-dissect-wireshark/
@@ -8,7 +8,7 @@ sudo cp -v ndn.lua /usr/local/share/ndn-dissect-wireshark/
 echo "dofile(\"/usr/local/share/ndn-dissect-wireshark/ndn.lua\")" | sudo tee -a /usr/share/wireshark/init.lua > /dev/null
 
 # Install python dependencies
-sudo pip3 install virtualenv
+sudo pip3 install virtualenv wheel pandas jupyter
 virtualenv icn-analysis
 source icn-analysis/bin/activate
 pip install pyshark matplotlib scipy numpy
