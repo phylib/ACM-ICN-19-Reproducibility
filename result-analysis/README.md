9359bba616871691cf1b96b0203868655c6092d5
phylib <phylib@users.noreply.github.com>
2019-07-30 14:19:33 +0200

    Add installation instructions and improve install.sh

---
+++ result-analysis/README.md
@@ -0,0 +1,10 @@
+# Analyse the emulation results
+
+In order to analyze the emulation results a Juypter file explaining the installation process was created. In order
+to launch the jupyter, install all required dependencies and start Jupyter.
+
+```bash
+./install.sh
+source icn-analysis/bin/activate
+jupyter notebook OverheadCalculation.ipynb
+```   
--- result-analysis/install.sh
+++ result-analysis/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Install wireshark and NDN dissector
-sudo apt install tshark wireshark python3-setuptools
+echo "Install wireshark and the ndn dissector"
+sudo apt install tshark wireshark
 git clone https://github.com/named-data/ndn-tools.git
 cd ndn-tools/tools/dissect-wireshark
 sudo mkdir /usr/local/share/ndn-dissect-wireshark/
@@ -8,10 +9,9 @@ sudo cp -v ndn.lua /usr/local/share/ndn-dissect-wireshark/
 echo "dofile(\"/usr/local/share/ndn-dissect-wireshark/ndn.lua\")" | sudo tee -a /usr/share/wireshark/init.lua > /dev/null
 
 # Install python dependencies
-sudo pip3 install virtualenv wheel pandas jupyter
+echo "Install python virtualenvironment and all dependencies in the icn-analysis environment"
+sudo apt install python3-pip python3-dev python3-setuptools
+sudo pip3 install virtualenv
 virtualenv icn-analysis
 source icn-analysis/bin/activate
-pip install pyshark matplotlib scipy numpy
-
-# Execute analysis
-#python3 ~/Coding/Minecraft/mc-server-sync/statistics/trafficAnalysis/parsePCAP.py -i final-results/ -o final-results/
+pip3 install wheel pandas jupyter pyshark matplotlib scipy numpy
