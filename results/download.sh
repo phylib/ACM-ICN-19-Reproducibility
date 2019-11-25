fe60446cc9f7a30f42d2d27f43035167d4216b5f
Philipp Moll <phylib@users.noreply.github.com>
2019-11-25 09:55:10 +0100

    Changed URI to FTP server

--- results/download.sh
+++ results/download.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd $ANALYSIS_HOME/results/
-wget -O final-results.tar.gz  http://www-itec.aau.at/ftp/icn/minecraft-ndn/ICN-19/final-results.tar.gz
+wget -O final-results.tar.gz  http://ftp.itec.aau.at/icn/minecraft-ndn/ICN-19/final-results.tar.gz
 tar -zxf final-results.tar.gz
